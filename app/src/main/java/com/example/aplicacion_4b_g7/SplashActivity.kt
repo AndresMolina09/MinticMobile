9a7ff7cd4a2ed5e76c6d05a2b3522bab9504b19b
jucemehu2020 <70982974+jucemehu2020@users.noreply.github.com>
2022-08-27 16:38:16 -0500

    v1.0
    Creación del modelo base, se han eliminado los archivos innecesarios.

---
+++ app/src/main/java/com/example/aplicacion_4b_g7/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.aplicacion_4b_g7
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.aplicacion_4b_g7.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
