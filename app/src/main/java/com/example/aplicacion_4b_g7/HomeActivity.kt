d9bb6a07b80926dc7cb8662dc566b1a4c04cc94a
jucemehu2020 <70982974+jucemehu2020@users.noreply.github.com>
2022-09-12 00:39:54 -0500

    Actualizacion
    Adelantado hasta la clase del lunes 5 de septiembre

--- app/src/main/java/com/example/aplicacion_4b_g7/HomeActivity.kt
+++ app/src/main/java/com/example/aplicacion_4b_g7/HomeActivity.kt
@@ -15,6 +15,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.homeToolbar)
     }
 
     override fun onStart() {
