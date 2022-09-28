2a30c9808be768e27fc2b63399a142484acaa4de
jucemehu2020 <70982974+jucemehu2020@users.noreply.github.com>
2022-09-28 12:27:06 -0500

    Clase 13 de septiembre - part1 funcionando

---
+++ app/src/main/java/com/example/aplicacion_4b_g7/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.aplicacion_4b_g7
+
+val CAMERA_PERMISSION = 100
+val TAKE_PICTURE = 200
--- app/src/main/java/com/example/aplicacion_4b_g7/ProfileFragment.kt
+++ app/src/main/java/com/example/aplicacion_4b_g7/ProfileFragment.kt
@@ -1,11 +1,18 @@
 package com.example.aplicacion_4b_g7
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.aplicacion_4b_g7.databinding.FragmentProfileBinding
+import com.google.android.material.snackbar.Snackbar
+import java.util.jar.Manifest
 
 class ProfileFragment : Fragment() {
 
@@ -18,5 +25,56 @@ class ProfileFragment : Fragment() {
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.profileFragmentName.text = "Nicolas Santander"
+        binding.profileFragmentEmail.text = "josenicolas@gmail.com"
+        binding.profileFragmentGender.text = "Masculino"
+        binding.profileFragmentLogout.setOnClickListener{
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+        binding.profileFragmentImage.setOnClickListener{
+            if(this.checkPermission(android.Manifest.permission.CAMERA, CAMERA_PERMISSION)){
+                openCamera()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == CAMERA_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            openCamera()
+        }else{
+            Snackbar.make(binding.root, "Permiso no otorgado", Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == TAKE_PICTURE){
+            if(data != null && data.extras != null) {
+                val extras = data.extras!!
+                val image = extras["data"] as Bitmap?
+                binding.profileFragmentImage.setImageBitmap(image)
+            }
+        }
+    }
+
+    private fun openCamera(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try{
+            startActivityForResult(intent, TAKE_PICTURE, )
+        }catch(e: ActivityNotFoundException){
+
+        }
     }
 }
