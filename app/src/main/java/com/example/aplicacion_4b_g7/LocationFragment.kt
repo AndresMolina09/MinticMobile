01b005a9b23dddf140c4ba0e1149eaa0beb3c7ed
jucemehu2020 <70982974+jucemehu2020@users.noreply.github.com>
2022-09-28 16:52:36 -0500

    Actualizacion al dia con la clase del 13 de septiembre

--- app/src/main/java/com/example/aplicacion_4b_g7/LocationFragment.kt
+++ app/src/main/java/com/example/aplicacion_4b_g7/LocationFragment.kt
@@ -6,12 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.aplicacion_4b_g7.databinding.FragmentLocationBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class LocationFragment : Fragment() {
+class LocationFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentLocationBinding? = null
     private val binding: FragmentLocationBinding get() = _binding!!
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +26,26 @@ class LocationFragment : Fragment() {
     ): View? {
         _binding = FragmentLocationBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val mapFragment: SupportMapFragment = childFragmentManager.findFragmentById(R.id.fragment_location_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        mMap = map
+        mMap.uiSettings.isScrollGesturesEnabled = true
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isZoomGesturesEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        mMap.isTrafficEnabled = true
+        val latLng: LatLng = LatLng(2.443377779866151, -76.60837714470695)
+        mMap.addMarker(MarkerOptions().position(latLng)
+            .title("MyMedic"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f))
+
     }
 }
